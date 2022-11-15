ac3384c1a12bdb0c6cf88678908fa27bc350e027
Michael Chop <007micle007@gmail.com>
2022-11-15 16:38:33 +0300

    Implemented image uploading functionality

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,16 +1,10 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
-import {validationResult} from "express-validator";
 
 import UserModel from "../models/User.js";
 
 export const register = async (req, res) => {
     try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
-        }
-
         const password = req.body.password
         // genSalt() - генерируем соль(алгоритм шифрования пароля)
         const salt = await bcrypt.genSalt(10)
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import multer from 'multer'
 import {loginValidation, postCreateValidation, registerValidation} from "./validations/validations.js";
 
 import checkAuth from "./utils/checkAuth.js";
@@ -7,6 +8,7 @@ import {key} from "./utils/MongoDB-key.js";
 
 import * as UserController from './controllers/UserController.js'
 import * as PostController from './controllers/PostController.js'
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 mongoose.connect(key)
     .then(() => console.log('DB Connected'))
@@ -15,26 +17,42 @@ mongoose.connect(key)
 const app = express()
 const PORT = process.env.PORT || 4444
 
-app.use(express.json())
+// Создаем хранилище для сохранения картинок
+const storage = multer.diskStorage({
+    // Путь для сохранения картинок
+    destination: (_, __, cb) => {
+        // Сохранить загруженные файлы в папку uploads (Путь для сохранения файла)
+        cb(null, 'uploads')
+    },
+    // Как будет называться файл
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
 
-app.post('/auth/login', loginValidation,  UserController.login)
-app.post('/auth/register', registerValidation, UserController.register)
+// Функция для использования хранилища 'storage'
+const upload = multer({ storage })
+
+app.use(express.json())
+// Если запрос на /uploads, то проверяем содержимое статической папки uploads
+app.use('/uploads', express.static('uploads'))
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
-// Получение всех статей | GET
+// upload.single('image') - Указываем, что мы ожидаем св-во image с картинкой
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
+
 app.get('/posts', PostController.getAll)
-
-// Получение конкретной статьи | GET
 app.get('/posts/:id', PostController.getOne)
-
-// Создание статьи | POST
-app.post('/posts', checkAuth, postCreateValidation, PostController.create)
-
-// Удаление статьи | DELETE
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-
-// Изменение статьи | PATCH
-app.patch('/posts/:id', checkAuth, PostController.update)
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
 
 app.listen(PORT, (err) => {
     if (err) {
