b02fa6df0f12f377c97584d9669d70e70db1c092
Michael Chop <007micle007@gmail.com>
2022-11-15 10:01:04 +0300

    Added JWT and MongoDB to the project

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+mongoose.connect('mongodb+srv://admin:12345@cluster0.f6mplrp.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB Connected'))
+    .catch((error) => console.log('DB Error: ', error))
 
 const app = express()
 const PORT = process.env.PORT || 4444
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello world')
+})
+
+app.post('/auth/login', (req, res) => {
+    // Генерируем token // В .sign({ инфо для шифрования }, ключ для шифрования)
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Chopsqd'
+    }, 'secretKey')
+
+    res.json({
+        success: true,
+        token
+    })
 })
 
 app.listen(PORT, (err) => {
